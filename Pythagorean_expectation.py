4eae08139bb878fd366f54b1093c5f471b9f19ee
TroyMcClure8998 <59133026+TroyMcClure8998@users.noreply.github.com>
2021-05-22 19:52:51 -0400

    Update Pythagorean_expectation.py

--- Pythagorean_expectation.py
+++ Pythagorean_expectation.py
@@ -1,6 +1,10 @@
+#Source: https://en.wikipedia.org/wiki/Pythagorean_expectation
+
 
 def pyt_exp(points_for, points_against):
-    win = (points_for ** 2)/ ((points_for ** 2) + points_against ** 2)
+    win = (points_for ** 13.91)/ ((points_for ** 13.91) + points_against ** 13.91)
     return win
 
 pyt_exp(921,640)
+
+
